6a4c0a088794495434f0d5d62b9342d2b10052d0
mosiur73 <mosiurrahman.cmt173@gmail.com>
2026-02-01 09:19:29 +0600

    login page complate

---
+++ src/app/(commonLayout)/login/page.tsx
@@ -0,0 +1,15 @@
+import { LoginForm } from "@/components/modules/authentication/login-form";
+
+
+
+export default function LoginPage(){
+    return(
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+                    <div className="w-full max-w-sm">
+                   {/* this is a login page */}
+                  <LoginForm></LoginForm>
+                    </div>
+                </div>
+       
+    )
+}
